ae8b49619473b96e95443e433ad10c77d29293a8
dannebrob <65211641+dannebrob@users.noreply.github.com>
2021-05-16 19:07:07 +0200

    fix of navbar

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const toggleBtn = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+const hidden = document.querySelector(".video");
 
 toggleBtn.addEventListener("click", function () {
  if (links.classList.contains("show-links")){
      links.classList.remove("show-links");
+     hidden.classList.remove("hidden");
  } else {
      links.classList.add("show-links");
+     hidden.classList.add("hidden");
  }
 });
 
